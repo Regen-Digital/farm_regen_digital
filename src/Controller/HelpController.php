df1d51d310f6865050f9fc10ef3bdce056bdd31e
Paul Weidner <paul.weidner@gmail.com>
2022-04-18 16:17:32 -0700

    Add help page.

---
+++ src/Controller/HelpController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\farm_regen_digital\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides a help page for farm_regen_digital.
+ */
+class HelpController extends ControllerBase {
+
+  /**
+   * Regen digital help page.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function helpPage() {
+    $output = [];
+    return $output;
+  }
+
+}
