90a6ce819563fe42a9eda0f642ab44e95d95b0e9
Paul Weidner <paul.weidner@gmail.com>
2022-11-16 11:13:20 -0800

    Add document asset

---
+++ modules/document/src/Plugin/Asset/AssetType/Document.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_regen_digital_document\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the document asset type.
+ *
+ * @AssetType(
+ *   id = "regen_digital_document",
+ *   label = @Translation("Document"),
+ * )
+ */
+class Document extends FarmAssetType {
+
+}
