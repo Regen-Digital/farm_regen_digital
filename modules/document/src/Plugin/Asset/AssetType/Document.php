f280f00f135d1d15358694de7ac99ae727d05116
Paul Weidner <paul.weidner@gmail.com>
2022-11-16 11:59:50 -0800

    Add related_asset field to document asset.

--- modules/document/src/Plugin/Asset/AssetType/Document.php
+++ modules/document/src/Plugin/Asset/AssetType/Document.php
@@ -14,4 +14,28 @@ use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
  */
 class Document extends FarmAssetType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'related_asset' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Related assets'),
+        'description' => $this->t('What assets is this document about?'),
+        'target_type' => 'asset',
+        'multiple' => TRUE,
+        'weight' => [
+          'form' => -5,
+          'view' => -5,
+        ],
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
